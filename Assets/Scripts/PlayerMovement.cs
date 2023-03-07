02600a548a740219e4aedb39dde2cfc25455ddaf
Mateo Jimenez Fama <mateojimenezfamaart@gmail.com>
2023-03-07 07:58:10 -0500

    PrimerComit
    Escena Mock, movimiento y disparo

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public CharacterController2D controller;
+
+    float horizontalMove = 0f;
+
+    public float runSpeed = 40f;
+    bool jump = false;
+    bool crouch;
+    
+
+    Animator animator;
+
+    void Update()
+    {
+       horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+        }
+
+        if (Input.GetButtonDown("Crouch"))
+        {
+            crouch = true;
+        } else if (Input.GetButtonUp("Crouch"))
+        {
+            crouch = false;
+        }
+
+    }
+
+    public void onCrouching(bool isCrouching)
+    {
+        animator.SetBool("IsCrouching", isCrouching);
+    }
+
+    private void FixedUpdate()
+    {
+        controller.Move(horizontalMove * Time.deltaTime,crouch,jump);  
+        jump = false;
+    }
+}
